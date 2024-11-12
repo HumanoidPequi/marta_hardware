cda0b053cd0a307eb8bff0209990057c37c71d78
Lucas Ólives <lucas.olives@discente.com>
2024-11-12 06:00:38 -0300

    setting limits to legs

--- sync_read_write/sync_read_write.ino
+++ sync_read_write/sync_read_write.ino
@@ -24,15 +24,39 @@ const uint8_t DXL_ID_CNT_ARM_L_HEAD = DXL_ID_CNT - 1;
 const uint8_t DXL_ID_LIST_R[DXL_ID_CNT] = {1,2,3,4,5,6};
 const uint8_t DXL_ID_LIST_L[DXL_ID_CNT] = {7,8,9,10,11,12};
 const uint8_t DXL_ID_LIST_ARM_R[DXL_ID_CNT_ARM_R] = {13,14,15};
-const uint8_t DXL_ID_LIST_ARM_L_HEAD[DXL_ID_CNT_ARM_L_HEAD] = {19,20,16,17,18};// 3 primeiros são do braço esquerdo e os dois últimos são da cabeça
+const uint8_t DXL_ID_LIST_ARM_L_HEAD[DXL_ID_CNT_ARM_L_HEAD] = {19,20,16,17,18};// 3 últimos são do braço esquerdo e os dois primeiros são da cabeça
 const int8_t Signal_left[DXL_ID_CNT] = {-1,-1,1,1,-1,1};
 const int8_t Signal_right[DXL_ID_CNT] = {-1,-1,-1,-1,1,1};
-const int8_t Signal_arm_r[DXL_ID_CNT_ARM_R] = {1,1,-1}; //ainda precisa ser definido
-const int8_t Signal_arm_l_head[DXL_ID_CNT_ARM_L_HEAD] = {-1,-1,1,-1,1}; //ainda precisa ser definido
+const int8_t Signal_arm_r[DXL_ID_CNT_ARM_R] = {1, 1, -1}; 
+const int8_t Signal_arm_l_head[DXL_ID_CNT_ARM_L_HEAD] = {1, 1, 1, -1, 1}; 
 const uint16_t user_pkt_buf_cap = 128;
 uint8_t user_pkt_buf1[user_pkt_buf_cap];
 uint8_t user_pkt_buf2[user_pkt_buf_cap];
 uint8_t user_pkt_buf3[user_pkt_buf_cap];
+
+//perna direita
+const int32_t rHipYaw  =    2048; //1
+const int32_t rHipRoll  =   2048; //2
+const int32_t rHipPitch  =  2048; //3
+const int32_t rKneePitch =  2048; //4
+const int32_t rAnklePitch = 2048; //5
+const int32_t rAnkleRoll =  2048;  //6
+
+//perna esquerda
+const int32_t lHipYaw   =   2048; //7
+const int32_t lHipRoll  =   2048; //8
+const int32_t lHipPitch =   2048; //9
+const int32_t lKneePitch =  2048; //10
+const int32_t lAnklePitch = 2048; //11
+const int32_t lAnkleRoll  = 2048; //12
+
+unsigned int minPosR[DXL_ID_CNT] = {rHipYaw-341, rHipRoll-148, rHipPitch-455, rKneePitch-1024, rAnklePitch-455, rAnkleRoll-341}; // angulos minimos (12Bits)
+                                  
+unsigned int minPosL[DXL_ID_CNT] = {lHipYaw-341, lHipRoll-455, lHipPitch-1024, lKneePitch-1024, lAnklePitch-455, lAnkleRoll-341}; // angulos minimos (12Bits)                              
+                                  
+unsigned int maxPosR[DXL_ID_CNT] = {rHipYaw+341, rHipRoll+455, rHipPitch+1024, rKneePitch+1024, rAnklePitch+455, rAnkleRoll+341};
+
+unsigned int maxPosL[DXL_ID_CNT] = {lHipYaw+341, lHipRoll+148, lHipPitch+455, lKneePitch+1024, lAnklePitch+455, lAnkleRoll+341};       
 
 // Starting address of the Data to read; Present Position = 132
 const uint16_t SR_START_ADDR = 132;
@@ -110,6 +134,13 @@ int32_t goal_position_arm_l_head[DXL_ID_CNT_ARM_L_HEAD] = {2048,2048,2048,2048,2
 
 int convert_command(int ang_command /*, int32_t marta_joints_initial, bool joint_reversed*/) {
   //Converte os valores de 16 bits recebidos pelo rosserial para os valores de 12 bits
+  int position_command = map(ang_command, 0, 63, -2048, 2048);
+  
+  return position_command;
+}
+
+int convert_command_degree(int ang_command /*, int32_t marta_joints_initial, bool joint_reversed*/) {
+  //Converte os valores de 16 bits recebidos pelo rosserial para os valores de 12 bits
   int position_command = map(ang_command, -1800, 1800, -2048, 2048);
   
   return position_command;
@@ -131,22 +162,24 @@ void right_leg_cb(const std_msgs::Int16MultiArray& cmd_msg) {
 
 void left_leg_cb(const std_msgs::Int16MultiArray& cmd_msg) {
    for (int i = 0; i < DXL_ID_CNT; i++) {
+   for (int i = 0; i < DXL_ID_CNT; i++) {
       int32_t d_theta = convert_command(cmd_msg.data[i]);
       goal_position_L[i] = goal_position_initial_L[i] + d_theta*Signal_left[i];
    }
-}
+  }
+} 
 
 void arm_r_cb(const std_msgs::Int16MultiArray& cmd_msg) {
    for (int i = 0; i < DXL_ID_CNT_ARM_R; i++) {
       int32_t d_theta = convert_command(cmd_msg.data[i]);
-      goal_position_arm_r[i] = goal_position_arm_r[i] + d_theta*Signal_arm_r[i];
+      goal_position_arm_r[i] = goal_position_initial_arm_r[i] + d_theta*Signal_arm_r[i];
    }
 }
 
 void arm_l_head_cb(const std_msgs::Int16MultiArray& cmd_msg) {
    for (int i = 0; i < DXL_ID_CNT_ARM_L_HEAD; i++) {
-      int32_t d_theta = convert_command(cmd_msg.data[i]);
-      goal_position_arm_l_head[i] = goal_position_arm_l_head[i] + d_theta*Signal_arm_l_head[i];
+      int32_t d_theta = convert_command_degree(cmd_msg.data[i]);
+      goal_position_arm_l_head[i] = goal_position_initial_arm_l_head[i] + d_theta*Signal_arm_l_head[i];
    }
 }
 
